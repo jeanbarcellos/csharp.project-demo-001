a6ee3319570b93b4339f097c561038ad274b924b
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-08-17 22:11:34 -0300

    Adição de validações no ViewModel

--- ViewModel/CategoryViewModel.cs
+++ ViewModel/CategoryViewModel.cs
@@ -1,8 +1,14 @@
-﻿namespace Demo.ViewModel
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Demo.ViewModel
 {
     public class CategoryViewModel
     {
         public int? Id { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [MinLength(3, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres")]
+        [MaxLength(128, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres")]
         public string Name { get; set; }
     }
 }
