256dd08ffa3af8537725bdf9b0d1bf86bbd5e9f3
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-08-23 21:50:41 -0300

    Criação dodos perfils para o automapper

---
+++ src/Demo.Api/AutoMapper/DomainToViewModelMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Demo.Api.ViewModel;
+using Demo.Domain.Entities;
+
+namespace Demo.Api.AutoMapper
+{
+    public class DomainToViewModelMappingProfile : Profile
+    {
+        public DomainToViewModelMappingProfile()
+        {
+            CreateMap<Category, CategoryViewModel>();
+        }
+    }
+}
---
+++ src/Demo.Api/AutoMapper/ViewModelToDomainMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Demo.Api.ViewModel;
+using Demo.Domain.Entities;
+
+namespace Demo.Api.AutoMapper
+{
+    public class ViewModelToDomainMappingProfile : Profile
+    {
+        public ViewModelToDomainMappingProfile()
+        {
+            CreateMap<CategoryViewModel, Category>();
+        }
+    }
+}
