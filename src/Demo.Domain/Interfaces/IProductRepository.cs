7432fc40cf9a437b73aadf26fc9e97d22072392f
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-08-23 22:14:57 -0300

    Domain - Criação da interface IProductRepository

---
+++ src/Demo.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using Demo.Core.Data;
+using Demo.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Demo.Domain.Interfaces
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+        Task<IEnumerable<Product>> GetAll();
+        Task<Product> GetById(int id);
+        Task<bool> Exists(int id);
+
+        int Insert(Product product);
+        void Update(Product product);
+        void Delete(Product product);
+        void Delete(int id);
+    }
+}
