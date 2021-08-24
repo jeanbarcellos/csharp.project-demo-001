f210a233c99f88f86507d6cc2e51229270231fe2
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-08-23 21:50:16 -0300

    Melhorias em CategoryViewModel

--- src/Demo.Api/ViewModel/CategoryViewModel.cs
+++ src/Demo.Api/ViewModel/CategoryViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Demo.Api.ViewModel
 {
@@ -10,5 +11,8 @@ namespace Demo.Api.ViewModel
         [MinLength(3, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres")]
         [MaxLength(128, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres")]
         public string Name { get; set; }
+
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
     }
 }
