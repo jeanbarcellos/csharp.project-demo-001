43813ddb851f37a42167591ece9ea1d005ca255f
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-08-23 21:53:09 -0300

    Data - Geração de novas migrations

---
+++ src/Demo.Data/Migrations/20210823234700_AddProductEntity.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Demo.Data.Migrations
+{
+    public partial class AddProductEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "product",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    name = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    description = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false),
+                    active = table.Column<bool>(type: "boolean", nullable: false),
+                    value = table.Column<decimal>(type: "numeric", nullable: false),
+                    quantity = table.Column<int>(type: "integer", nullable: false),
+                    image = table.Column<string>(type: "text", nullable: true),
+                    created_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    category_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("product_pk", x => x.id);
+                    table.ForeignKey(
+                        name: "product_category_id_fk",
+                        column: x => x.category_id,
+                        principalTable: "category",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_product_category_id",
+                table: "product",
+                column: "category_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "product");
+        }
+    }
+}
